e8fab708d18f9f83ca98bccf8057fc92818110ba
amrit <amritpoudel433@gmail.com>
2025-01-21 18:32:19 +0545

    test

--- lib/sessions.ts
+++ lib/sessions.ts
@@ -29,7 +29,7 @@ export async function CreateSession(userId : string){
      cookieStore.set('session' , session , {
         httpOnly : true ,
         expires : expiresAt ,
-        secure : true,
+        secure : false,
      })
 }
 
