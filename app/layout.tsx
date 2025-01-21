c44c081d7c1f3b425f12841895dddc85cc607eeb
amrit <amritpoudel433@gmail.com>
2025-01-21 13:08:27 +0545

    build error fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import HeaderComponent from '@/components/header/page'
-import  CommonLayoutpage from '@/components/commonLayout/page'
+import  CommonLayoutpage from '../components/commonLayout/page'
 
 
 const geistSans = Geist({
